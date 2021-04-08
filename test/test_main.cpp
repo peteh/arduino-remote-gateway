ab9d19511afba4d9e4afa9150a9c9b5c52e82bea
Peter Hofmann <peter.hofmann@kuka.com>
2021-04-08 13:22:37 +0800

    ADD: tests

---
+++ test/test_main.cpp
@@ -0,0 +1,30 @@
+#include <unity.h>
+#include "helpers.h"
+
+// void setUp(void) {
+// // set stuff up here
+// }
+
+// void tearDown(void) {
+// // clean stuff up here
+// }
+
+void testDipCodeParse_whenWrongArea_thenFail(void) {
+    // arrange
+    char topic[] = "rfdevices/station1/fail/1239434953/switch";
+    char code1[12];
+    char code2[12];
+    // act
+    bool result = extractDipCodeFromTopic(topic, code1, code2);
+
+    // assert
+    TEST_ASSERT_FALSE(result);
+}
+
+int main(int argc, char **argv) {
+    UNITY_BEGIN();
+    RUN_TEST(testDipCodeParse_whenWrongArea_thenFail);
+    UNITY_END();
+
+    return 0;
+}
