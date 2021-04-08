fadd9de848334b62d257878f6e3d599428768796
Peter Hofmann <peter.hofmann@kuka.com>
2021-04-08 13:42:35 +0800

    ADD: header guard

--- src/helpers.h
+++ src/helpers.h
@@ -1,6 +1,10 @@
+#ifndef HELPERS_H
+#define HELPERS_H
 enum switch_type { dip, codeid, unknown };
 
 bool extractDipCodeFromTopic(char* _topic, char* code1_, char* code2_);
 bool extractCodeIdFromTopic(char* topic, long *codeid);
 
 switch_type getSwitchType(char* topic);
+
+#endif
